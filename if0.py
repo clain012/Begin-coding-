1d2c32b0263be997d805874f0087c96df06baaca
Clain012 <mutebic256@gmail.com>
2026-08-26 12:43:59 +0300

    Delete if0.py

--- if0.py
+++
@@ -1,9 +0,0 @@
-temperature=float(input("what's your thermometers reading: "))
-
-if temperature >30:
-    print("a hot day")
-    print("Drink water")
-elif temperature < 30:
-    print("Cool of the day")
-    print( "Try something warm")
-
